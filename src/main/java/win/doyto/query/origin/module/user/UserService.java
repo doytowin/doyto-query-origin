7673f18de30ecc6df17b3bef4803a8893b3fc520
Forb Yuan <f0rb@163.com>
2021-10-01 12:21:58 +0800

    Refactor query: replace UserSpecification.java with UserQueryBuilder

--- src/main/java/win/doyto/query/origin/module/user/UserService.java
+++ src/main/java/win/doyto/query/origin/module/user/UserService.java
@@ -4,8 +4,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.Resource;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
 
 /**
  * UserService
@@ -13,13 +16,26 @@ import javax.annotation.Resource;
  * @author f0rb on 2021-10-01
  */
 @Service
+@SuppressWarnings("unchecked")
 public class UserService {
 
     @Resource
     UserRepository userRepository;
 
+    @Resource
+    EntityManager entityManager;
+
+    UserQueryBuilder userQueryBuilder = new UserQueryBuilder();
+
     public List<User> query(UserQuery userQuery) {
-        return userRepository.findAll(new UserSpecification(userQuery));
+        ArrayList<Object> argList = new ArrayList<>();
+        String sql = userQueryBuilder.buildSelectAndArgs(userQuery, argList);
+
+        Query query = entityManager.createNativeQuery(sql, User.class);
+        for (int i = 0; i < argList.size(); i++) {
+            query.setParameter(i + 1, argList.get(i));
+        }
+        return query.getResultList();
     }
 
     public Page<User> page(UserQuery userQuery) {
