f1d38f8e19aa023b383f771bd7fc7af0eb2d0b2f
Forb Yuan <f0rb@163.com>
2021-10-01 12:14:02 +0800

    A typical spring data jpa Specification usage

---
+++ src/test/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO t_user (id, account, valid) VALUES (1, 'admin', true);
+INSERT INTO t_user (id, account, valid) VALUES (2, 'test1', true);
+INSERT INTO t_user (id, account, valid) VALUES (3, 'test2', false);
