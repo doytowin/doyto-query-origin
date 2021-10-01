4e9621d306e36ff9e2c4a168594141bcf834a30e
Forb Yuan <f0rb@163.com>
2021-10-01 12:17:23 +0800

    Add page logic

---
+++ src/main/java/win/doyto/query/origin/query/PageQuery.java
@@ -0,0 +1,41 @@
+package win.doyto.query.origin.query;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.domain.PageRequest;
+
+import java.io.Serializable;
+
+/**
+ * PageQuery
+ *
+ * @author f0rb on 2021-10-01
+ */
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PageQuery implements Serializable {
+    private Integer pageNumber;
+    private Integer pageSize;
+
+    public PageRequest toPageRequest() {
+        return PageRequest.of(getPageNumber(), getPageSize());
+    }
+
+    public int getPageNumber() {
+        if (pageNumber == null || pageNumber < 0) {
+            return 0;
+        }
+        return pageNumber;
+    }
+
+    public int getPageSize() {
+        if (pageSize == null || pageSize < 0) {
+            return 10;
+        }
+        return pageSize;
+    }
+}
